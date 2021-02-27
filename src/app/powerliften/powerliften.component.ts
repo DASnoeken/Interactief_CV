abd33bf4bc3ca7d5104ab1b66ae0dfcc33ebb574
Daan Snoeken <d.a.snoeken@protonmail.com>
2021-02-27 20:52:20 +0100

    havamal sections

--- src/app/powerliften/powerliften.component.ts
+++ src/app/powerliften/powerliften.component.ts
@@ -18,16 +18,14 @@ export class PowerliftenComponent {
   }
   mouseDown(id: string) {
     document.getElementById(id).style.transition = "all 0.2s";
-    document.getElementById(id).style.width = "900px";
-    document.getElementById(id).style.height = "506px";
+    document.getElementById(id).style.width = "150%";
     document.getElementById(id).style.zIndex = "100";
     document.getElementById(id).style.boxShadow = "0 18px 25px 0 rgba(0, 0, 0, 1), 0 20px 60px 0 rgba(0, 0, 0, 0.5), 0 40px 80px 0 rgba(0,0,0,0.1)";
   }
 
   mouseUp(id: string) {
     document.getElementById(id).style.transition = "all 0.2s";
-    document.getElementById(id).style.width = "600px";
-    document.getElementById(id).style.height = "337px";
+    document.getElementById(id).style.width = "90%";
     document.getElementById(id).style.zIndex = "1";
     document.getElementById(id).style.boxShadow = "0 0px 0px 0 rgba(0, 0, 0, 0)";
   }
